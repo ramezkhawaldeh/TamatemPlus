bb4966e8811596cde823ba7bf1e270e00696408c
Ramez Khawaldeh <ramezkhawaldeh@outlook.com>
2023-07-11 07:52:28 +0300

    added initial VC with routing button

--- Tamatem+/ViewController.swift
+++ Tamatem+/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class InitialViewController: UIViewController {
 
+    lazy var openBrowserButton: UIButton = {
+        let button = UIButton()
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(button)
+
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive = true
+        button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+
+        button.addTarget(self, action: #selector(didPressOpenBrowserButton), for: .touchUpInside)
+       
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupOpenBrowserButtonTitle()
+    }
+    
+    private func setupOpenBrowserButtonTitle() {
+        openBrowserButton.setTitle("Open Browser", for: .normal)
+        openBrowserButton.setTitleColor(.blue, for: .normal)
     }
 
+    @objc func didPressOpenBrowserButton() {
+  
+    }
 
 }
 
